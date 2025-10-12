ade49272b0a4a71eef490781542db86e0f25b8d6
guialveess <97guilherme.alves@gmail.com>
2025-10-12 15:39:30 -0300

    Update about.tsx

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -2,6 +2,8 @@ import { createFileRoute } from "@tanstack/react-router";
 import { Header } from "@/components/ui/header";
 import { Instrument_Serif } from "@/fonts/instrumentSerif";
 import { ImageDithering } from "@paper-design/shaders-react";
+import { useEffect, useState } from "react";
+import { Dithering } from "@paper-design/shaders-react";
 
 const instrumentSerif = Instrument_Serif({
   subsets: ["latin"],
@@ -14,6 +16,20 @@ export const Route = createFileRoute("/about")({
 });
 
 function RouteComponent() {
+  const [isFirefox, setFirefox] = useState(false);
+
+  useEffect(() => {
+    if (typeof navigator !== "undefined") {
+      const ua = navigator.userAgent.toLowerCase();
+      const isChromeBrowser =
+        ua.includes("firefox") && !ua.includes("edge") && !ua.includes("opr");
+      if (isChromeBrowser) {
+        setFirefox(true);
+        console.log("Você está usando o Firefox");
+      }
+    }
+  }, []);
+
   return (
     <div className="relative w-screen h-screen overflow-hidden">
       <Header />
@@ -33,7 +49,26 @@ function RouteComponent() {
           />
         </div>
 
-        <section className="relative z-10 cursor-pointer transition-all duration-300"></section>
+        <section className="relative z-10 transition-all duration-700">
+          <Dithering
+            width={1280}
+            height={720}
+            colorBack="rgba(0,0,0,0)"
+            colorFront="#ff4800"
+            shape="sphere"
+            type="4x4"
+            size={2}
+            speed={1}
+            scale={0.6}
+          />
+
+          {isFirefox && (
+            <p className="p-3 flex rounded-xl text-md transition-all font-mono duration-700 text-center border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50">
+              Opps, unfortunately Firefox is not yet enabled for this page, try
+              another browser
+            </p>
+          )}
+        </section>
       </main>
     </div>
   );
