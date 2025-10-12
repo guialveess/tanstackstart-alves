3b6a023b137558f374fe4cfad03eae662eca31ae
guialveess <97guilherme.alves@gmail.com>
2025-10-12 15:02:38 -0300

    feat: paper design ImageDithering in about page

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -1,6 +1,7 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { Header } from "@/components/ui/header";
 import { Instrument_Serif } from "@/fonts/instrumentSerif";
+import { ImageDithering } from "@paper-design/shaders-react";
 
 const instrumentSerif = Instrument_Serif({
   subsets: ["latin"],
@@ -18,38 +19,21 @@ function RouteComponent() {
       <Header />
 
       <main className="relative flex-1 flex items-center justify-center h-full w-full">
-        <img
-          src="/img/G2r4MlMXIAAC3s2.jpeg"
-          alt="Imagem sobre"
-          className="absolute inset-0 w-full h-full object-cover"
-        />
+        <div className="absolute inset-0">
+          <ImageDithering
+            width={window.innerWidth}
+            height={window.innerHeight}
+            image="/img/G2r4MlMXIAAC3s2.jpeg"
+            colorBack="#000000"
+            colorFront="#2c2a26"
+            colorHighlight="#ccff00"
+            originalColors={false}
+            size={2}
+            colorSteps={2}
+          />
+        </div>
 
-        <section className="relative z-10 cursor-pointer transition-all duration-300">
-          <div
-            className={`flex flex-col items-center space-y-2 transition-opacity duration-300`}
-          >
-            <h1
-              className={`${instrumentSerif.className} text-2xl font-bold text-white transition-transform duration-300 hover:scale-110`}
-            >
-              C
-            </h1>
-            <h1
-              className={`${instrumentSerif.className} text-2xl font-bold text-white transition-transform duration-300 hover:scale-110`}
-            >
-              A
-            </h1>
-            <h1
-              className={`${instrumentSerif.className} text-2xl font-bold text-white transition-transform duration-300 hover:scale-110`}
-            >
-              O
-            </h1>
-            <h1
-              className={`${instrumentSerif.className} text-2xl font-bold text-white transition-transform duration-300 hover:scale-110`}
-            >
-              S
-            </h1>
-          </div>
-        </section>
+        <section className="relative z-10 cursor-pointer transition-all duration-300"></section>
       </main>
     </div>
   );
